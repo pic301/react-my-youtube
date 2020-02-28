c851ab28def628c4a5e802091cb19b5879dd19d1
pic301 <ppic301@gmail.com>
2020-02-29 03:48:06 +0900

    동영상 Upload 기능 구현
    multer , Dropzone 라이브러리를 이용해서
    서버에 저장

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,43 +1,59 @@
 import React from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from 'axios'
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+
+const onDrop = (files) =>{
+  let formData = new FormData();
+  const config = {
+      header: {'content-type': 'multipart/form-data'}
+  }
+  formData.append("file", files[0])
+  console.log(files)
+
+  Axios.post('/api/video/uploadfiles',formData,config).then((response) => {
+     if(response.data.success){
+        console.log(response.data)
+    } else{
+      alert('비디오 업로드 실패')
+    }
+  })
+}
+
 const VideoUploadPage = () => {
   return (
     <div
-      style={{
-        maxWidth: "700px",
-        margin: "2rem auto",
-        border: "5px solid red"
-      }}
-    >
+       style={{
+         maxWidth: "700px",
+         margin: "2rem auto",
+         border: "5px solid red"
+       }}
+     >
       <div style={{ textAlign: "center" }}>
         <Title levle={2}> Upload Video </Title>
       </div>
       <Form onSubmit>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
+         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* 드랍존 */}
-          <Dropzone onDrop maxSize multiple>
-            {({ getRootProps, getInputProps }) => (
-              <div
-                style={{
-                  width: "300px",
-                  height: "240px",
-                  border: "1px solid lightgray",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center"
-                }}
-                {...getRootProps()}
-              >
-                <input {...getInputProps()} />
-                <Icon type="plus" style={{ fontSize: "3rem" }} />
-              </div>
-            )}
-          </Dropzone>
+          <Dropzone
+                    onDrop={onDrop}
+                    multiple={false}
+                    maxSize={1000000000}
+                    >
+                        {
+                            ({getRootProps,getInputProps}) => (
+                                <div style={{width: '300px', height: '240px', border: '1px solid lightgray',display:"flex",
+                                alignItems: 'center', justifyContent: 'center'}}{...getRootProps()}>
+                                      <input {...getInputProps()}/>
+                                      <Icon type="plus" style={{ fontSize: "3rem" }} />
+                                </div>
+                            )
+                        }
+                    </Dropzone>
           {/* 썸네일 */}
           <img src alt />
         </div>
@@ -69,3 +85,5 @@ const VideoUploadPage = () => {
 };
 
 export default VideoUploadPage;
+
+
