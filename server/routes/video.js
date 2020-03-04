451d12965cf00a50ec56c469e502f0b7e7c9c8d3
pic301 <ppic301@gmail.com>
2020-03-05 04:29:08 +0900

    비디오 업로드 하기

--- server/routes/video.js
+++ server/routes/video.js
@@ -41,6 +41,15 @@ router.post("/uploadfiles", (req, res) => {
 
 });
 
+router.get('/getVideos', (req,res)=>{
+    //비디오를 db에서 가져와서 클라이언트로 보낸다
+    Video.find().populate('writer')
+    .exec((err, videos) =>{
+        if(err) return res.status(400).send(err);
+        res.status(200).json({ success: true, videos})
+    });
+})
+
 
 router.post("/thumbnail", (req, res) => {
 
